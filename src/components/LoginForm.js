4833b3868f2118c636c7a771cda63da3d11efa81
HangCcZ <hangchen1128@gmail.com>
2020-11-22 20:37:56 -0500

    replaced form with Formik

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,44 +1,74 @@
 import React from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { Form, Button } from "react-bootstrap"
+import { Form as styleForm, Button } from "react-bootstrap"
 import { loginUser } from "../reducers/userReducer"
-
-import { inputUsername, inputPassword } from "../reducers/loginInfoReducer"
+import { Formik, Field, Form } from "formik"
 
 const LoginForm = () => {
-  const { username, password } = useSelector((state) => state.loginInfo)
+  const user = useSelector((state) => state.user)
   const dispatch = useDispatch()
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
-    dispatch(loginUser(username, password))
+  const onFormSubmit = async (values, { resetForm, setSubmitting }) => {
+    setSubmitting(true)
+    dispatch(loginUser(values.username, values.password))
+    setSubmitting(false)
+    if (user === null) {
+      resetForm({ values: { username: values.username, password: "" } })
+    } else {
+      resetForm({ values: { username: "", password: "" } })
+    }
   }
+
   return (
-    <Form onSubmit={handleLogin}>
-      <Form.Group>
-        <Form.Label>Username:</Form.Label>
+    <div>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={onFormSubmit}
+      >
+        {({ values, isSubmitting }) => (
+          <Form>
+            <styleForm.Group>
+              <styleForm.Label>Username:</styleForm.Label>
+              <Field name='username' type='input' as={styleForm.Control} />
+            </styleForm.Group>
 
-        <Form.Control
-          type='text'
-          value={username}
-          id='username'
-          name='Username'
-          onChange={({ target }) => dispatch(inputUsername(target.value))}
-        />
+            <styleForm.Group>
+              <styleForm.Label>Password:</styleForm.Label>
+              <Field name='password' type='password' as={styleForm.Control} />
+            </styleForm.Group>
 
-        <Form.Label>Password:</Form.Label>
-        <Form.Control
-          type='password'
-          id='password'
-          value={password}
-          name='Password'
-          onChange={({ target }) => dispatch(inputPassword(target.value))}
-        />
-      </Form.Group>
-      <Button variant='primary' type='submit'>
-        Log In
-      </Button>
-    </Form>
+            <Button variant='primary' type='submit'>
+              Sign In
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+    // <Form onSubmit={handleLogin}>
+    //   <Form.Group>
+    //     <Form.Label>Username:</Form.Label>
+
+    //     <Form.Control
+    //       type='text'
+    //       value={username}
+    //       id='username'
+    //       name='Username'
+    //       onChange={({ target }) => dispatch(inputUsername(target.value))}
+    //     />
+
+    //     <Form.Label>Password:</Form.Label>
+    //     <Form.Control
+    //       type='password'
+    //       id='password'
+    //       value={password}
+    //       name='Password'
+    //       onChange={({ target }) => dispatch(inputPassword(target.value))}
+    //     />
+    //   </Form.Group>
+    //   <Button variant='primary' type='submit'>
+    //     Log In
+    //   </Button>
+    // </Form>
   )
 }
 
