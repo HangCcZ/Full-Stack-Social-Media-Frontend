a7a9c8d52eb7f5abcadf832382ecf0db18ed2e90
HangCcZ <hangchen1128@gmail.com>
2020-11-23 16:44:18 -0500

    added signup validation

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -4,6 +4,36 @@ import signUpService from "../services/signup"
 
 import { Formik, Field, Form } from "formik"
 import { useHistory } from "react-router-dom"
+import * as Yup from "yup"
+
+const SignupSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(8, "Too Short!")
+    .max(15, "Too Long!")
+    .required("Required"),
+  username: Yup.string()
+    .min(8, "Too Short!")
+    .max(15, "Too Long!")
+    .required("Required"),
+  password: Yup.string()
+    .min(8, "Too Short!")
+    .max(15, "Too Long")
+    .required("Required"),
+  confirmPassoword: Yup.string()
+    .min(8, "Too Short!")
+    .max(15, "Too Long")
+    .required("Required"),
+})
+
+const validatePasswords = (values) => {
+  const errors = {}
+  if (values.password !== values.confirmPassword) {
+    errors.confirmPassword = "Must be the same as password above"
+  }
+  console.log("error is", errors)
+  return errors
+}
+
 const SignUpForm = () => {
   const history = useHistory()
   return (
@@ -13,10 +43,12 @@ const SignUpForm = () => {
           name: "",
           username: "",
           password: "",
-          confirmPassowrd: "",
+          confirmPassword: "",
         }}
+        validate={validatePasswords}
+        validationSchema={SignupSchema}
         onSubmit={async (values, { resetForm, setSubmitting }) => {
-          if (values.password === values.confirmPassowrd) {
+          if (values.password === values.confirmPassword) {
             setSubmitting(true)
             await signUpService.signUp({
               name: values.name,
@@ -29,40 +61,64 @@ const SignUpForm = () => {
                 name: "",
                 username: "",
                 password: "",
-                confirmPassowrd: "",
+                confirmPassword: "",
               },
             })
             history.push("/")
-          } else {
-            /* TODO */
-            console.log("bad match")
           }
         }}
       >
-        {({ values, isSubmitting }) => (
+        {({ values, errors, touched }) => (
           <Form>
             <styleForm.Group>
               <styleForm.Label>Full Name:</styleForm.Label>
-              <Field name='name' type='input' as={styleForm.Control} />
+              <Field
+                name='name'
+                type='input'
+                as={styleForm.Control}
+                autoComplete='off'
+              />
+              {errors.name && touched.name ? <div>{errors.name}</div> : null}
             </styleForm.Group>
 
             <styleForm.Group>
               <styleForm.Label>Username:</styleForm.Label>
-              <Field name='username' type='input' as={styleForm.Control} />
+              <Field
+                name='username'
+                type='input'
+                as={styleForm.Control}
+                autoComplete='off'
+              />
+              {errors.username && touched.username ? (
+                <div>{errors.username}</div>
+              ) : null}
             </styleForm.Group>
 
             <styleForm.Group>
               <styleForm.Label>Password:</styleForm.Label>
-              <Field name='password' type='password' as={styleForm.Control} />
+              <Field
+                name='password'
+                type='password'
+                as={styleForm.Control}
+                autoComplete='off'
+              />
+              {errors.password && touched.password ? (
+                <div>{errors.password}</div>
+              ) : null}
             </styleForm.Group>
 
             <styleForm.Group>
               <styleForm.Label>Confirm Password:</styleForm.Label>
               <Field
                 type='password'
-                name='confirmPassowrd'
+                name='confirmPassword'
                 as={styleForm.Control}
+                autoComplete='off'
               />
+              {errors.confirmPassword &&
+              (values.confirmPassword !== "" || touched.confirmPassword) ? (
+                <div>{errors.confirmPassword}</div>
+              ) : null}
             </styleForm.Group>
 
             <Button variant='primary' type='submit'>
