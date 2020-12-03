8a4cee118354b1eeff60b7e68c14343e1ba096dd
HangCcZ <hangchen1128@gmail.com>
2020-12-03 10:14:35 -0500

    update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,16 @@
 import React from "react"
 import "../index.css"
 export default function Footer() {
-  return <footer className='footer'>Hang Chen © 2020</footer>
+  return (
+    <footer className='footer'>
+      <a
+        href='https://github.com/HangCcZ'
+        target='_blank'
+        rel='noopener noreferrer'
+        style={{ color: "inherit" }}
+      >
+        App developed by Hang Chen
+      </a>
+    </footer>
+  )
 }
