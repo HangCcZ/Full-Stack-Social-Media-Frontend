9b7071dc00bc00f2dd8db6b4ae542fec5495e10e
HangCcZ <hangchen1128@gmail.com>
2020-12-14 15:39:13 -0500

    modified Button text based on sorting selection

--- src/components/SortButton.js
+++ src/components/SortButton.js
@@ -1,20 +1,24 @@
 import React from "react"
 import { DropdownButton, Dropdown } from "react-bootstrap"
 import "../index.css"
-const SortButton = ({ showSortButton, setSortBy }) => {
+const SortButton = ({ showSortButton, setSortBy, sortBy }) => {
   return (
     <div className={showSortButton === true ? "displaySortButton" : "hide"}>
-      <DropdownButton id='dropdown-basic-button' title='SORT BY' variant='dark'>
+      <DropdownButton
+        id='dropdown-basic-button'
+        title={`SORT BY: ${sortBy}`}
+        variant='dark'
+      >
         <Dropdown.Item
           onClick={() => {
-            setSortBy("NEWEST_OLDEST")
+            setSortBy("Newest to Oldest")
           }}
         >
           Newest to Oldest
         </Dropdown.Item>
         <Dropdown.Item
           onClick={() => {
-            setSortBy("OLDEST_NEWEST")
+            setSortBy("Oldest to Newest")
           }}
         >
           Oldest to Newest
@@ -22,14 +26,14 @@ const SortButton = ({ showSortButton, setSortBy }) => {
 
         <Dropdown.Item
           onClick={() => {
-            setSortBy("LEAST_MOST_LIKES")
+            setSortBy("Least to Most Likes")
           }}
         >
           Least Likes to Most Likes
         </Dropdown.Item>
         <Dropdown.Item
           onClick={() => {
-            setSortBy("MOST_LEAST_LIKES")
+            setSortBy("Most to Least Likes")
           }}
         >
           Most Likes to Least Likes
